a9713174198498df6b4a3e4fa054b392b661f65f
Nadav Kaufman <nadav1252@gmail.com>
2025-11-21 22:33:05 +0200

    hw

--- Primes.java
+++ Primes.java
@@ -1,7 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]), j = 2,  p = 2, count = 0 ;
-        System.out.println("Prime numbers up to " + n);
+        System.out.println("Prime numbers up to " + n + ":");
         boolean [] arr = new boolean [n+1];
         arr [0] = false;
         arr [1] = false;
